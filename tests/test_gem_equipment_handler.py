a4e1a2597837a02110d97b59a876f4167894dabc
Farhan Mustar <farhanmustar@gmail.com>
2021-07-26 19:24:59 +0800

    Fix duplicate test function name.

--- tests/test_gem_equipment_handler.py
+++ tests/test_gem_equipment_handler.py
@@ -1770,8 +1770,8 @@ class TestGemEquipmentHandlerPassiveControlState(unittest.TestCase):
         self.assertFalse(clientCommandThread.is_alive())
 
         self.assertFalse(self.client.alarms[25].set)
-        
-    def testAlarmTriggerOnCollectionEvent(self):
+
+    def testAlarmTriggerOnCollectionEventWithEnabledAlarm(self):
         self.setupTestAlarms()
         self.establishCommunication()
 
